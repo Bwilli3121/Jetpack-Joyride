30ce1c1155bae09e5e66f8779802fca528528516
Andrej Rinkovsky <85352343+Andrej084@users.noreply.github.com>
2026-04-21 13:37:26 -0400

    Update leddec16.vhd

--- leddec16.vhd
+++ leddec16.vhd
@@ -1,43 +1,44 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
-ENTITY leddec16 IS
+ENTITY bin2bcd IS
     PORT (
-        dig : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-        data : IN STD_LOGIC_VECTOR (15 DOWNTO 0);
-        anode : OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-        seg : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
+        bin  : IN  STD_LOGIC_VECTOR (11 DOWNTO 0);  -- 12-bit binary (0-4095)
+        bcd  : OUT STD_LOGIC_VECTOR (15 DOWNTO 0)   -- 4 BCD digits (16 bits)
     );
-END leddec16;
+END bin2bcd;
 
-ARCHITECTURE Behavioral OF leddec16 IS
-    SIGNAL data4 : STD_LOGIC_VECTOR (3 DOWNTO 0);
+ARCHITECTURE Behavioral OF bin2bcd IS
 BEGIN
-data4 <= "000" & data(0) WHEN dig = "000" ELSE
-         "000" & data(1) WHEN dig = "001" ELSE
-         "000" & data(2) WHEN dig = "010" ELSE
-         "000" & data(3);
+    PROCESS (bin)
+        VARIABLE temp : UNSIGNED (11 DOWNTO 0);
+        VARIABLE bcd_var : UNSIGNED (15 DOWNTO 0);
+    BEGIN
+        temp := UNSIGNED(bin);
+        bcd_var := (OTHERS => '0');
 
-    seg <= "0000001" WHEN data4 = "0000" ELSE
-           "1001111" WHEN data4 = "0001" ELSE
-           "0010010" WHEN data4 = "0010" ELSE
-           "0000110" WHEN data4 = "0011" ELSE
-           "1001100" WHEN data4 = "0100" ELSE
-           "0100100" WHEN data4 = "0101" ELSE
-           "0100000" WHEN data4 = "0110" ELSE
-           "0001111" WHEN data4 = "0111" ELSE
-           "0000000" WHEN data4 = "1000" ELSE
-           "0000100" WHEN data4 = "1001" ELSE
-           "0001000" WHEN data4 = "1010" ELSE
-           "1100000" WHEN data4 = "1011" ELSE
-           "0110001" WHEN data4 = "1100" ELSE
-           "1000010" WHEN data4 = "1101" ELSE
-           "0110000" WHEN data4 = "1110" ELSE
-           "0111000";
+        -- 12 iterations: one per input bit
+        FOR i IN 11 DOWNTO 0 LOOP
+            -- If any BCD digit >= 5, add 3 before shifting
+            IF bcd_var(3 DOWNTO 0) >= 5 THEN
+                bcd_var(3 DOWNTO 0) := bcd_var(3 DOWNTO 0) + 3;
+            END IF;
+            IF bcd_var(7 DOWNTO 4) >= 5 THEN
+                bcd_var(7 DOWNTO 4) := bcd_var(7 DOWNTO 4) + 3;
+            END IF;
+            IF bcd_var(11 DOWNTO 8) >= 5 THEN
+                bcd_var(11 DOWNTO 8) := bcd_var(11 DOWNTO 8) + 3;
+            END IF;
+            IF bcd_var(15 DOWNTO 12) >= 5 THEN
+                bcd_var(15 DOWNTO 12) := bcd_var(15 DOWNTO 12) + 3;
+            END IF;
 
-    anode <= "11111110" WHEN dig = "000" ELSE
-             "11111101" WHEN dig = "001" ELSE
-             "11111011" WHEN dig = "010" ELSE
-             "11110111" WHEN dig = "011" ELSE
-             "11111111";
+            -- Shift left: push next binary bit into LSB
+            bcd_var := bcd_var(14 DOWNTO 0) & temp(11);
+            temp := temp(10 DOWNTO 0) & '0';
+        END LOOP;
+
+        bcd <= STD_LOGIC_VECTOR(bcd_var);
+    END PROCESS;
 END Behavioral;
